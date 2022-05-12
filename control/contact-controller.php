cda718c2b11055f99314973af2c325cddedc010f
Oliver Holst <oliver.holst@sonova.com>
2022-05-12 13:40:45 +0200

    Corrected error message by alerting via javascript

--- control/contact-controller.php
+++ control/contact-controller.php
@@ -16,17 +16,17 @@ function SetId(int $id){
     fclose($idFile);
 }
 
-
-//TODO: Validate all the below
 //name can contain space but no special charackters. 
 $name = htmlspecialchars($_REQUEST['name']);
 $email = htmlspecialchars($_REQUEST['email']); 
 $message = htmlspecialchars($_REQUEST['message']); 
 
+$error = false;
 
 $nameregex = '/^[A-ZÜÖÄ][a-züöä]*$/';
 if (!preg_match($nameregex, $name)) {
     echo 'Name is not valid';
+    $error = true;
 }
 
 
@@ -34,6 +34,7 @@ $emailregex = '/^[^@\s]+@[^@\s]+\.[^@\s]+$/';
 if (($email != NULL) || ($email != "")) {
     if (!preg_match($emailregex, $email)) {
         echo 'Email is not valid';
+        $error = true;
     }
 }
 
@@ -42,16 +43,22 @@ $messageregex = '/^[A-Za-z0-9\s\,\.\?\!\:\;\(\)\-\_\+\=\@\#\$\%\&\*\[\]\{\}\"\'\
 if (($message != NULL) || ($message != "")) {
     if (!preg_match($messageregex, $message)) {
         echo 'Message is not valid';
+        $error = true;
     }
+
 }
 
-//Writing
-include './state-file-controller.php'; 
-$id = GetId(); 
-appendStateFile('../model/state_files/user_messages.txt',  $id.'^'. $name.'^'.$email.'^'.$message.';'); 
-$filename = '../model/state_files/user_messages.txt'; 
-$filestream = fopen($filename, 'a+'); 
-SetId(++$id); 
-header('location:'.$_SESSION['currPage']);
+if ($error == false) {
+    //Writing
+    include './state-file-controller.php'; 
+    $id = GetId(); 
+    appendStateFile('../model/state_files/user_messages.txt',  $id.'^'. $name.'^'.$email.'^'.$message.';'); 
+    $filename = '../model/state_files/user_messages.txt'; 
+    $filestream = fopen($filename, 'a+'); 
+    SetId(++$id); 
+    header('location:'.$_SESSION['currPage']);
+} else {
+    echo '<script>alert("The data you entered in the formular is invalid!");</script>';
+}
 
 ?>
